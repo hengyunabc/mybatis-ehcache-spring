27daefacdcd558e807fa65d225e0c0a4c5034bec
hengyunabc <hengyunabc@gmail.com>
2015-06-11 11:11:28 +0800

    MybatisMetricsEhcacheFactory add SharedMetricRegistries support.

--- src/main/java/io/github/hengyunabc/mybatis/MybatisMetricsEhcacheFactory.java
+++ src/main/java/io/github/hengyunabc/mybatis/MybatisMetricsEhcacheFactory.java
@@ -1,15 +1,27 @@
 package io.github.hengyunabc.mybatis;
 
+import javax.annotation.PostConstruct;
+
 import net.sf.ehcache.Ehcache;
 import net.sf.ehcache.config.CacheConfiguration;
 
 import org.apache.ibatis.cache.Cache;
 
 import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.SharedMetricRegistries;
 import com.codahale.metrics.ehcache.InstrumentedEhcache;
 
 public class MybatisMetricsEhcacheFactory extends MybatisEhcacheFactory {
   MetricRegistry registry;
+
+  String sharedMetricRegistryName;
+
+  @PostConstruct
+  public void init() {
+    if (registry == null && sharedMetricRegistryName != null) {
+      registry = SharedMetricRegistries.getOrCreate(sharedMetricRegistryName);
+    }
+  }
 
   @Override
   public Cache getCache(String id) {
@@ -39,5 +51,13 @@ public class MybatisMetricsEhcacheFactory extends MybatisEhcacheFactory {
 
   public void setRegistry(MetricRegistry registry) {
     this.registry = registry;
+  }
+
+  public String getSharedMetricRegistryName() {
+    return sharedMetricRegistryName;
+  }
+
+  public void setSharedMetricRegistryName(String sharedMetricRegistryName) {
+    this.sharedMetricRegistryName = sharedMetricRegistryName;
   }
 }
